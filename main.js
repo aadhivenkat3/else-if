0f4632070f631bfff9f138f5ebe12437ceacae65
aadhivenkat3 <sharmavenkat1@gmail.com>
2023-07-12 12:30:18 +0530

    add

---
+++ main.js
@@ -0,0 +1,13 @@
+// js condition elseif
+
+let num=prompt("enter your number");
+
+if(num>0){
+	document.write("positive");
+}
+else if(num==0){
+	document.write("given number is 0");
+}
+else{
+	document.write("negative");
+}
